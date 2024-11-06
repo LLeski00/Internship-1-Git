3e4aa50517d8e4d823ba85ea8b5caa114eda4efa
Luka <llesko00@fesb.hr>
2024-11-06 16:50:28 +0100

    Permutations

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include "permutations.hpp"
 
 void clear_screen() {
     #ifdef _WIN32
@@ -12,18 +14,24 @@ void clear_screen() {
 int main() {
     int program = 0;
     bool shutdown = false;
+    std::string buffer;
     
     do {
         clear_screen();
         std::cout << "Program list:" << std::endl;
+        std::cout << "2. Permutations" << std::endl;
         std::cout << "0. Exit" << std::endl;
         std::cout << "Which program would you like to run? : ";
         std::cin >> program;
+        std::getline(std::cin, buffer);
         
         switch(program) {
             case 0:
                 std::cout << "Goodbye!" << std::endl;
                 shutdown = true;
+                break;
+            case 2:
+                permutations_init();
                 break;
             default:
                 std::cout << "Wrong input!" << std::endl;
